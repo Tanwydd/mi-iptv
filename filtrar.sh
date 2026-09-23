aafabb0d9659db7edeaee1cc6849c4e3efaeff54
JABP <86165689+Tanwydd@users.noreply.github.com>
2026-09-23 10:44:53 +0200

    Update filter conditions in filtrar.sh

--- filtrar.sh
+++ filtrar.sh
@@ -5,7 +5,7 @@ curl -sL "https://www.tdtchannels.com/lists/tv.m3u8" -o /tmp/tdt.m3u8
 {
   echo "#EXTM3U"
   awk '/#EXTINF/{inf=$0; getline url;
-    if (inf ~ /group-title="(Nacionales|Andalucía)"/ ||
+    if (inf ~ /group-title="(Generalistas|Andalucía)"/ ||
         inf ~ /Condavisión|Doñana|Teleonuba|CanalCosta|Huelva/)
       print inf "\n" url }' /tmp/tdt.m3u8
 } > mi_lista.m3u8
